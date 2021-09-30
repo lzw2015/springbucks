ad4c7fb96d233639a802135c21dd99847cbcd104
lzw <lzw20150719@163.com>
2021-09-30 08:25:34 +0800

    add jpa complex demo

---
+++ jpa-complex-demo/src/main/java/com/st/learning/springbucks/jpa/JpaComplexDemoApplication.java
@@ -0,0 +1,13 @@
+package com.st.learning.springbucks.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaComplexDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaComplexDemoApplication.class, args);
+	}
+
+}
