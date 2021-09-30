e26cb25afc1329486d4b00e15b9580cb72f58e33
lzw <lzw20150719@163.com>
2021-09-30 14:10:15 +0800

    add mybatis generator demo

---
+++ mybatis-generator-demo/src/main/java/com/st/learning/springbucks/mybatis/handler/MoneyTypeHandler.java
@@ -0,0 +1,42 @@
+package com.st.learning.springbucks.mybatis.handler;
+
+import org.apache.ibatis.type.BaseTypeHandler;
+import org.apache.ibatis.type.JdbcType;
+import org.joda.money.CurrencyUnit;
+import org.joda.money.Money;
+
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * @Description
+ * @Author lizhiwei
+ * @Date 2021/9/30 上午9:26
+ */
+public class MoneyTypeHandler extends BaseTypeHandler<Money> {
+    @Override
+    public void setNonNullParameter(PreparedStatement preparedStatement, int i, Money money, JdbcType jdbcType) throws SQLException {
+        preparedStatement.setLong(i, money.getAmountMinorLong());
+    }
+
+    @Override
+    public Money getNullableResult(ResultSet resultSet, String s) throws SQLException {
+        return parseMoney(resultSet.getLong(s));
+    }
+
+    @Override
+    public Money getNullableResult(ResultSet resultSet, int i) throws SQLException {
+        return parseMoney(resultSet.getLong(i));
+    }
+
+    @Override
+    public Money getNullableResult(CallableStatement callableStatement, int i) throws SQLException {
+        return parseMoney(callableStatement.getLong(i));
+    }
+
+    private Money parseMoney(Long value) {
+        return Money.ofMinor(CurrencyUnit.of("CNY"), value);
+    }
+}
